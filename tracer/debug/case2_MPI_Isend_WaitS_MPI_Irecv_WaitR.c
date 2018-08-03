238f60284e02cce772275930a3ba08755a9c15bb
sramesh <sramesh@head.ri2.cse.ohio-state.edu>
2018-08-03 10:10:31 -0400

    Refactor some benchmarks

--- tracer/debug/case2_MPI_Isend_WaitS_MPI_Irecv_WaitR.c
+++ tracer/debug/case2_MPI_Isend_WaitS_MPI_Irecv_WaitR.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<mpi.h>
 
+#if WRITE_OTF2_TRACE
+#include <scorep/SCOREP_User.h>
+#endif
+
+#include "microbenchmarks.h"
+
 #define DIM 25
 static float **a = NULL, *x = NULL, *y = NULL;
 
@@ -63,6 +69,9 @@ main(int argc, char **argv) {
 	double starttime, endtime;
 
 	MPI_Init(&argc, &argv);
+#if WRITE_OTF2_TRACE
+  SCOREP_RECORDING_OFF();
+#endif
 	MPI_Comm_size(MPI_COMM_WORLD, &num);
 	if(num != 2) {
 		printf("Example must be run with 2 processes only.\n");
@@ -77,19 +86,23 @@ main(int argc, char **argv) {
 	init_arrays();
 
 	starttime = MPI_Wtime();
-
-	for(i=0; i < 30; i++) {
+#if WRITE_OTF2_TRACE
+  SCOREP_RECORDING_ON();
+#endif
+ 
+        MPI_Barrier(MPI_COMM_WORLD);
+	for(i=0; i < NUM_ITERS; i++) {
 
 	if(my_rank == 1) {
 	//Send
 		MPI_Isend(buffer, 100000000, MPI_INT, 0, 123, MPI_COMM_WORLD, &req);
-		compute(0.06);
+		compute(COMPUTE_TIME);
 		MPI_Wait(&req, &stat);
 	} else if(my_rank == 0) {
 	//Recv
-                compute(0.08);
+                compute(WAIT_TIME);
 		MPI_Irecv(buffer, 100000000, MPI_INT, 1, 123, MPI_COMM_WORLD, &req2);
-                compute(0.06);
+                compute(COMPUTE_TIME);
                 MPI_Wait(&req2, &stat2);
 	}
 
@@ -97,6 +110,9 @@ main(int argc, char **argv) {
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
+#if WRITE_OTF2_TRACE
+  SCOREP_RECORDING_OFF();
+#endif
 	endtime = MPI_Wtime();
 
 	if(my_rank == 0) printf("Done in %f seconds.\n", endtime - starttime);
